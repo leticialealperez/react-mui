c2c4557e4330f19c5ff83464bad54fc387596c19
Leticia Leal <leticialealp@hotmail.com>
2025-01-28 20:41:45 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { CssBaseline } from "@mui/material";
+import { ButtonUsage } from "./components/Button";
+
+export function App() {
+  return (
+    <>
+      <CssBaseline />
+      <h1>Vite + React</h1>
+      <ButtonUsage />
+    </>
+  );
+}
